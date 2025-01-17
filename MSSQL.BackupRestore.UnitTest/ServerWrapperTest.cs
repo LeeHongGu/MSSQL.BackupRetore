3600f7e333cfe9633e561501c8a5214c9d6abb99
honggu <honggu.lee@pixoneer.co.kr>
2025-01-17 09:43:04 +0900

    Add Unit Test Project and Update Comment
    - The 'MSSQL.BackupRestore.UnitTest' project was added to the file 'MSSQL.BackupRestore.sln', which is for unit testing.
    - Annotation for class 'ServerWrapper' has been added to the file 'ServerWrapper.cs ', which describes the functionality of the class and method.
    - A new 'MSSQL.BackupRestore.UnitTest.csproj' file has been added, setting up a unit test project, targeting .NET 8.0 and referencing several NuGet packages ('coverlet.collector', 'Microsoft.NET.Test.Sdk', 'NUnit.Analyzers', and 'NUnit3TestAdapter'.
    - A new file 'ServerWrapperTest.cs ' has been added, which includes a unit test for the 'ServerWrapper' class, including basic test settings and sample test methods.

---
+++ MSSQL.BackupRestore.UnitTest/ServerWrapperTest.cs
@@ -0,0 +1,16 @@
+namespace MSSQL.BackupRestore.UnitTest
+{
+    public class ServerWrapperTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
