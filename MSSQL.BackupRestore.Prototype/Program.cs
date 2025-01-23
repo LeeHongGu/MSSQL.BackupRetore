586803544d2dc5cc812577d83f9fd5ff261b3d8d
honggu <honggu.lee@pixoneer.co.kr>
2025-01-23 13:49:56 +0900

    Adding MSSQL.BackupRestore Solutions and Projects
    The 'MSSQL.BackupRestore.prototype' project was added to the 'MSSQL.BackupRestore.sln' file.
    
    The build settings for the project 'MSSQL.BackupRestore.Prototype' were added to the 'Global' section of the 'MSSQL.BackupRestore.sln' file.
    
    A new file 'MSSQL.BackupRestore.Prototype.csproj' was added.
    This file is targeted at .NET 8.0 and refers to the 'Microsoft.Extensions.Logging.Abstractions' package. It also refers to the 'MSSQL.BackupRestore' project.
    
    A new file 'Program.cs ' has been added, which includes the ability to output logs to the console using the 'ConsoleLoggerFactory' and 'ConsoleLogger' classes.

---
+++ MSSQL.BackupRestore.Prototype/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Logging;
+using MSSQL.BackupRestore.Works.RestoreWorks;
+
+namespace MSSQL.BackupRestore.Prototype
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var consoleLog = new ConsoleLoggerFactory();
+            var log = consoleLog.CreateLogger<RecoveryJob>();
+        }
+    }
+
+    public class ConsoleLoggerFactory : ILoggerFactory
+    {
+        public void AddProvider(ILoggerProvider provider)
+        {
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new ConsoleLogger();
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+
+    public class ConsoleLogger : ILogger
+    {
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return null!;
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            Console.WriteLine(formatter(state, exception));
+        }
+    }
+}
