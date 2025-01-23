55a746ad126c562720ce0287fd00f779fb49f471
honggu <honggu.lee@pixoneer.co.kr>
2025-01-23 11:34:52 +0900

    Change to Using Real SQL Server for Integrated Testing
    Modified the test to interact with a real SQL Server instance by changing 'Mock<Server>' to a real 'Server' object in the 'ServerWrapperTest.cs ' file. Added logic to create and delete test databases.
    
    A new 'DatabaseWrapperTest.cs ' file is added to perform an integrated test of the 'DatabaseWrapper' class using a real SQL Server instance. Validates functions such as creating, deleting, and verifying existence of databases.
    
    A new 'DatabaseWrapper.cs ' file has been added to provide a class that wraps the 'Microsoft.SqlServer.Management.Smo.Database' object.This class provides functions such as creating, deleting, and checking the existence of databases.
    
    A new 'IDatabase.cs ' file is added to define the interface implemented by the 'DatabaseWrapper' class. The interface includes the database name, presence verification, creation, deletion, etc.

---
+++ MSSQL.BackupRestore/Interfaces/IDatabase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSSQL.BackupRestore.Interfaces
+{
+    public interface IDatabase
+    {
+        string Name { get; }
+        bool Exists();
+        void Create();
+        void Drop();
+    }
+}
